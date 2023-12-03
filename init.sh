446bc18a423b7007ce1192579d5cfea8cf78fdd0
Tim Reynolds <tim@cubingusa.org>
2023-12-03 00:35:28 -0500

    Add comment to init.sh.

--- init.sh
+++ init.sh
@@ -1,2 +1,8 @@
+# Usage: For day 1,
+#
+# ./init.sh 1 session=$SESSION_COOKIE
+#
+# where $SESSION_COOKIE is grabbed from Chrome.
+
 cp -r template $1
 curl --cookie $2 https://adventofcode.com/2023/day/$1/input > $1/input.txt
