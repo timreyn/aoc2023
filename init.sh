61de743d33d5b27ce1889016a169c380f3077abf
Tim Reynolds <tim@cubingusa.org>
2023-12-06 06:27:19 -0500

    .txt rather than .md

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@
 # where $SESSION_COOKIE is grabbed from Chrome.
 
 cp -r template $1
-echo "Start: $(date)" > $1/times.md
+echo "Start: $(date)" > $1/times.txt
 curl --cookie $2 https://adventofcode.com/2023/day/$1/input > $1/input.txt
